919d3d2f4ca71187d17cfbc708e90c869b41dff8
Victor <victorrodrigues288@gmail.com>
2024-08-15 22:10:23 -0300

    alguns testes

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Aula02</title>
+</head>
+<body>
+
+
+    <!-- <div class="corpo"> 
+
+    <?php 
+
+    $nota1 = 7;
+    $nota2 = 10;
+    $nota3 = 5;
+    $nota4 = 4;
+
+    $notaSemestral = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
+
+    if ($notaSemestral >= 6){
+        echo "<h2 class='nota'> Nota: $notaSemestral <h3 class='nota' style='color:'>APROVADO";
+    }
+
+    else{
+        echo "<h2 class='nota'> Nota $notaSemestral <h3 class='nota' style='color:'>REPROVADO";
+    }
+
+    ?>
+
+    <br>
+
+    <?php
+
+    $mes = 3;
+
+    switch ($mes) {
+        
+        case 1:
+            echo "Janeiro";
+            break;
+
+            case 2:
+                echo "fevereiro";
+                break;
+
+                case 3:
+                    echo "<h2 class='mes'> Março";
+                    break;
+
+        default:
+        echo "mês invalido";
+    }
+
+    ?>
+
+    <?php
+
+    // Laços de repetição
+
+    $i = 7;
+
+    //for ($i; $i <=10; $i++){
+
+   // }
+
+    while ($i >=10){
+        echo "<h1> Teste While";
+    }
+
+    ?>
+
+</div> -->
+
+<div class="pares">
+
+<?php
+
+$num = 0;
+
+for ($num; $num <=100; $num++) {
+
+    if ($num % 2 == 0) {
+
+        echo "<h2 class='numpar' style='background-color: blue'> $num";
+    }
+
+    else {
+        echo "<h2 class='numpar' style='background-color: purple'> $num";
+    }
+    
+
+}
+
+
+
+?>
+
+
+
+</div>
+
+<div class="tabuada">
+
+    <?php
+
+    $tab = 1;
+    $num = 5;
+
+    for ($tab; $tab<=10; $tab++){
+
+        $resultado = $num * $tab;
+        
+        echo "5 x $tab = $resultado <br />";
+    }
+
+    ?>
+
+</div>
+
+</div>
+
+</body>
+</html>
